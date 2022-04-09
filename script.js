ca011f004ee3442ef6c942631c64823192e6e766
K93MSY <85401098+K93MSY@users.noreply.github.com>
2022-04-10 08:42:01 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const music = new Audio('Itro & Tobu - Cloud 9 [NCS Release].mp3');
+let musicstuation=1;
+
+function btgithub(){
+console.log('github!')
+iziToast.success({ title: 'Github Loaded', message: 'load success!' });
+window.location.href = 'https://github.com/K93MSY/';
+}
+
+function btdocument(){
+console.log('open document');
+iziToast.success({ title: 'Document Loaded', message: 'in preparation' });
+}
+
+function btmusic(){
+if (musicstuation===1){
+console.log('playing music');
+iziToast.success({ title: 'Music Loaded', message: 'Playing Itro & Tobu - Cloud 9 [NCS Release]' });
+music.play();
+musicstuation =0;
+}else{
+if (musicstuation===0){
+console.log('pause music');
+iziToast.success({ title: 'Music stopped', message: 'Pausing' });
+music.pause();
+musicstuation =1;
+}else{
+console.log('error!')
+}}}
